c72851a2f51f895767ce1935881a59e658cdae4f
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-02-17 09:42:22 -0800

    Create test for fourth spec, word appears multiple times in string

--- Tests/RepeatCounterTest.cs
+++ Tests/RepeatCounterTest.cs
@@ -37,5 +37,16 @@ namespace WordCounter
             int expectedResult = 1;
             Assert.Equal(expectedResult, result);
         }
+
+        [Fact]
+        public void CountRepeats_ForWordInStringMultipleTimes_wordcount()
+        {
+            string userInputString = "What's better than coding? Nothing is better than coding.";
+            string userInputWord = "coding";
+            RepeatCounter testRepeatCounter = new RepeatCounter(userInputString, userInputWord);
+            int result = testRepeatCounter.CountRepeats();
+            int expectedResult = 2;
+            Assert.Equal(expectedResult, result);
+        }
     }
 }
