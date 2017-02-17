c338a4ac231aae8d0ac06a16ecd72ecde584bdc4
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-02-17 13:15:01 -0800

    Style count page

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -10,6 +10,13 @@ namespace WordCounter
             Get["/"] = _ => {
                 return View["index.cshtml"];
             };
+
+            Post["/count"] = _ => {
+                string userString = Request.Form["user-input-string"];
+                string userWord = Request.Form["user-input-word"];
+                RepeatCounter newCount = new RepeatCounter(userString, userWord);
+                return View["count.cshtml", newCount];
+            };
         }
     }
 }
