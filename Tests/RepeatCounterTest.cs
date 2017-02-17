ab989d4b0ede01f9564c60ac4b1455f918c5a6c7
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-02-17 09:35:34 -0800

    Add test for second spec

--- Tests/RepeatCounterTest.cs
+++ Tests/RepeatCounterTest.cs
@@ -15,5 +15,16 @@ namespace WordCounter
             int expectedResult = 0;
             Assert.Equal(expectedResult, result);
         }
+
+        [Fact]
+        public void CountRepeats_ForWordIsString_1()
+        {
+            string userInputString = "bananas";
+            string userInputWord = "bananas";
+            RepeatCounter testRepeatCounter = new RepeatCounter(userInputString, userInputWord);
+            int result = testRepeatCounter.CountRepeats();
+            int expectedResult = 1;
+            Assert.Equal(expectedResult, result);
+        }
     }
 }
